c7c5baaaef6a9d6e5a47bdd02dd1755d2e45b0b9
MiaPass <86061223+MiaPass@users.noreply.github.com>
2022-01-05 12:36:53 -0300

    arreglos

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,15 +1,14 @@
-const {Router} = require('express')
+const { Router } = require("express");
 
 //import all routers;
-const Admin = require('./admin')
-const Posts = require('./posts')
-const User = require('./user')
+const Admin = require("./admin");
+const Posts = require("./posts");
+const User = require("./user");
 
 const router = Router();
 
-router.use('/admin', Admin);
-router.use('./posts', Posts);
-rotuer.use('./user', User)
+router.use("/admin", Admin);
+router.use("/posts", Posts);
+router.use("/user", User);
 
-
-module.exports =router;
+module.exports = router;
