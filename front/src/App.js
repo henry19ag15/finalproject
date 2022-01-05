c80eebcc0410896a2e3c93f5fac55cc3c237367a
gonzaqepasa <gonzalovam@hotmail.com>
2022-01-04 21:52:16 -0300

    Mergea la config de firebase

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Register from './components/Register/Register';
+// import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      {/* <Register/> */}
 
     </div>
   );
