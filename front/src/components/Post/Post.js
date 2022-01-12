264e20a3b02b9874bc3c2e090f3c89cc80dd0ccf
DaniCancino <dacr270492@gmail.com>
2022-01-12 11:45:00 -0300

    Compone post precarga imagen

--- front/src/components/Post/Post.js
+++ front/src/components/Post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './Post.css';
 import Modal from '../Modal/Modal';
 import {FaRegPlusSquare } from "react-icons/fa";
@@ -6,7 +6,8 @@ import useModal  from '../../customHooks/useModal';
 import { GrCamera } from "react-icons/gr";
 
 const Post = () =>{
-    const [isOpenModal, openModal, closeModal] = useModal();
+    const [isOpenModal, openModal, closeModal, fileUrl, noFile, processImage] = useModal();  
+
 
     return(
         <div className='Post'>
@@ -15,11 +16,17 @@ const Post = () =>{
                 <div className='post-container'>
                     <h2>Crear Publicación</h2>
                     <form className='form-post'>
-                        <div className='img-container'>
-                              <GrCamera />      
+                        <div className={fileUrl ? 'img-container' : 'no-image' }>
+                              <GrCamera className='camera-icon'/>
+                              <img src={fileUrl ? fileUrl : noFile} alt=''/>
                         </div>
                         <div className='btn-upload'>
-                            <input name='file' type='file' className='upload-file' accept='image/*'></input>
+                            <input name='file'
+                                   type='file'
+                                   className='upload-file'
+                                   accept='image/*'
+                                   onChange={processImage}
+                            />
                             <label htmlFor='file'>Seleccionar foto</label>
                         </div>
                         {/* <textarea placeholder='Agregar descripcion' className='description'></textarea>
--- front/src/customHooks/useModal.js
+++ front/src/customHooks/useModal.js
@@ -2,12 +2,26 @@ import { useState } from "react";
 
 const useModal = (initialValue = false) => {
   const [isOpen, setIsOpen] = useState(initialValue);
-  
+  const [fileUrl, setFileUrl] = useState(null);
+  const [noFile, setNofile] = useState(null);
+  console.log(fileUrl)
+
+
   const openModal = () => setIsOpen(true);
 
-  const closeModal = () => setIsOpen(false);
+  const closeModal = () => {
+    setIsOpen(false);
+    const imageUrl = URL.createObjectURL(noFile);
+    setNofile(imageUrl)
+  }
 
-  return [isOpen, openModal, closeModal];
+  const processImage =(event) =>{
+    const imageFile = event.target.files[0];
+    const imageUrl = URL.createObjectURL(imageFile);
+    setFileUrl(imageUrl)
+ }
+
+  return [isOpen, openModal, closeModal, fileUrl, noFile, processImage];
 };
 
 export default useModal
