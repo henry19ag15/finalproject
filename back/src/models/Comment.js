1c0028b235f7a04a0ef117bcea1844d7ffa8d026
Dylan Juresa Furlong <dylanjuresa@gmail.com>
2022-01-14 00:00:52 -0300

    push heroku nuevas rutas(posts/likes/comments)

--- back/src/models/Comment.js
+++ back/src/models/Comment.js
@@ -1,29 +1,29 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "comment",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-      },
-      data: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: true,
-      },
-      creator: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-    }
-    // {
-    //   timestamps: false,
-    // }
-  );
+    sequelize.define(
+        "comment",
+        {
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true,
+            },
+            detail: {
+                type:DataTypes.STRING,
+                allowNull:true
+            },
+            idUser: {
+                type:DataTypes.STRING,
+                allowNull:true
+            },
+            idPost: {
+                type:DataTypes.STRING,
+                allowNull:true
+            }
+        }
+        // {
+        //   timestamps: false,
+        // }
+    );
 };
