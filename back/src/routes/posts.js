b468f38d09ccf24669f6a0bbac96526d68f70833
Dylan Juresa Furlong <dylanjuresa@gmail.com>
2022-01-14 01:24:15 -0300

    push para heroku -back listo

--- back/src/routes/posts.js
+++ back/src/routes/posts.js
@@ -65,47 +65,7 @@ server.put("/likes", async (req, res) => {
 
     }
 });
-// comentar
-server.post("/comments", async function (req, res) {
 
-    try {
-        const {
-            idUser,
-            idPost,
-            detail,
-        } = req.body;
-
-        await Comment.create({
-            detail: detail,
-            idUser: idUser,
-            idPost: idPost,
-
-        })
-        res.status(200).send("comentario")
-
-    } catch (error) {
-        console.log(error)
-
-    }
-
-
-});
-// eliminar comentario
-server.delete("/commentdelete/:id", async function (req, res) {
-    try {
-        const { id } = req.params;
-        await Comment.destroy({
-            where: {
-                id,
-            },
-        });
-        res.status(200).send("Comentario eliminado correctamente");
-    } catch (error) {
-        console.log(error);
-    }
-
-
-});
 
 
 module.exports = server;
