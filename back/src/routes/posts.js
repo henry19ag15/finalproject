4c6c428a026f1ed64dc9153d525c8ef1f8ecdf76
rgcivit <86114994+rgcivit@users.noreply.github.com>
2022-01-16 18:13:55 -0300

    Ruta traer comentarios

--- back/src/routes/posts.js
+++ back/src/routes/posts.js
@@ -19,15 +19,16 @@ server.post('/', async function (req, res) {
 
 
 })
-//traerse los posts por usuarios
-server.post('/getbyusers', async function (req, res) {
 
+//traerse los posts por usuarios
+server.get('/getbyusers', async function (req, res) {
+    console.log(req.body)
 
     let posts = [];
     try {
         posts =  await Post.findAll({
             where: {
-                creator:req.body.payload.map(e => e)
+                creator:req.body.map(e => e)
             }
 
         })
@@ -44,8 +45,13 @@ server.post('/getbyusers', async function (req, res) {
 });
 
 
+
+
+
 // traerse todos los post
 server.get('/getAll', async function (req, res) {
+
+
     try {
         let posts = await Post.findAll();
         res.send(posts);
@@ -90,57 +96,63 @@ server.put("/likes", async (req, res) => {
 
     }
 });
+// Realizar  comentarios
+server.post("/comments", async function (req, res) {
 
-//borrar post
-
-server.delete("/destroy/:id", async function (req, res) {
     try {
-      const { id } = req.params;
-      await Post.destroy({
-        where:{
-          id
-        }
-        
-      });
-      res.status(200).send("Post eliminado correctamente");
+        const {
+            idUser,
+            idPost,
+            detail,
+        } = req.body;
+
+        await Comment.create({
+            detail: detail,
+            idUser: idUser,
+            idPost: idPost,
+
+        })
+        res.status(200).send("Comentario creado con exito")
+
     } catch (error) {
-      console.log(error);
+        console.log(error)
+
+    }
+
+
+});
+
+//Traer un comentario
+
+server.get("/bringscomments/:id", async function (req, res) {
+    try {
+      let user = await Comment.findOne({
+        where: { id: req.params.id },
+      });
+      res.status(200).send(user);
+    } catch (error) {
+      res.status(400).json({
+        error: true,
+        message: "Error al buscar comentario",
+      });
     }
   });
-
-  //editar post
-
-  server.put('/setting/:id',(req, res, next)=>{
-    console.log(req.body)
-    const { id, detail} = req.body.payload;
-    
-    var postmod = {
-      photo, 
-      detail:detail,
-      creator,
-      likes,
-      active 
+// eliminar comentario
+server.delete("/commentdelete/:id", async function (req, res) {
+    try {
+        const { id } = req.params;
+        await Comment.destroy({
+            where: {
+                id,
+            },
+        });
+        res.status(200).send("Comentario eliminado correctamente");
+    } catch (error) {
+        console.log(error);
     }
-    Post.findOne({
-      where:{
-        id:id
-      }
-    }).then(post => {
-      post.update(postmod)
-      .then(newPost =>{
-        newPost.save()
-        res.status(200).send('Post modificado con exito')
-        return res.json(newPost)
-      }).catch(error => { console.log(error) })
-      
-   }).catch(err => {
-     console.log(err)
-     res.status(404).send('Post no encontrado')
-   })
-  })
 
 
-
+});
 
 
 module.exports = server;
