6556ed3398cd097785326b06bc0639f0a6a27c6c
gonzaqepasa <gonzalovam@hotmail.com>
2022-01-27 05:27:36 -0300

    Se corrigio bug de redireccion

--- front/src/components/LikesModal/LikesModal.js
+++ front/src/components/LikesModal/LikesModal.js
@@ -2,24 +2,42 @@ import React from "react";
 import "./LikesModal.css";
 import { useDispatch, useSelector } from "react-redux";
 import noimg from '../../sass/noimg.png';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import { getUserProfile } from "../../Redux/02-actions";
+import { getAuth } from "firebase/auth";
 
 const LikesModal = ({ likes, setOpenLike, creator }) => {
   const dispatch = useDispatch();
   const users = useSelector(state => state.allUser)
+  const history = useHistory()
+  const auth = getAuth()
 
-  
 
-  const user= (userID) =>{
+  const user = (userID) => {
     const userToUse = users.filter(us => us.id === userID)
 
+
+    function redirect() {
+
+      if (userID === auth.currentUser.uid) {
+        history.push("/profile")
+      } else {
+
+
+        history.push(`/user/${userID}`)
+        dispatch(getUserProfile([userID]))
+      }
+    }
+
+
     return (
-        <div className ='user-cointainer'>
-            <Link className='Link' to={`/user/${userID}`}>
-               {userToUse[0]?.profilephoto ? <img src={userToUse[0]?.profilephoto} alt='' className ='imagen'></img> : <img src={noimg} alt=''></img> }
-                <p className='username'>{userToUse[0]?.username}</p>
-            </Link>
-        </div>
+      <div className='user-cointainer'>
+        <button className='Link' onClick={() => { redirect() }} >
+          {userToUse[0]?.profilephoto ? <img src={userToUse[0]?.profilephoto} alt='' className='imagen'></img> : <img src={noimg} alt=''></img>}
+          <p className='username'>{userToUse[0]?.username}</p>
+        </button>
+      </div>
     )
   }
 
